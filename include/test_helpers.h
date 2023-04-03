33e6f4b2c50c4a0a228ecc9609ecdd2abe800411
M. Valin <mfvalin@gmail.com>
2023-04-03 08:59:46 -0400

    test program start/stop messages

---
+++ include/test_helpers.h
@@ -0,0 +1,3 @@
+
+void end_of_test(void) ;
+void start_of_test(char *name) ;
---
+++ tests/test_helpers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void end_of_test(void){
+  fprintf(stderr,"==================== end of test  ==================== ") ;
+  system("date '+%Y-%m-%d_%H.%M.%S' 1>&2") ;
+}
+
+void start_of_test(char *name){
+  fprintf(stderr,"==================== test : %s ====================\n", name) ;
+  atexit(end_of_test) ;
+}
+void start_of_test_(char *name){ start_of_test(name) ; };
+void start_of_test__(char *name){ start_of_test(name) ; };
