1361418f01669fc620f53d8afc36b109b7253d1c
M. Valin <mfvalin@gmail.com>
2023-04-18 19:25:54 -0400

    phase 1 extra bindings for Fortran

--- tests/test_c_record_io.F90
+++ tests/test_c_record_io.F90
@@ -8,14 +8,31 @@ program test_c_record
   integer :: ndim, fd, ndata
   integer, dimension(3) :: dims
   type(C_PTR) :: pbufw, pbufr
+  integer, dimension(:,:,:), pointer :: rbuf
 
   dims = [1, 2, 3]
   ndim = 3
+  buf = [10, 11, 12, 13, 14, 15]
   pbufw = C_LOC(buf(1))
   call start_of_test("test c record"//achar(0))
   fd = 0
   ndata = write_32bit_data_record("./temporary_file"//achar(0), fd, [1, 2, 3], 3, pbufw)
   print *,'number of items written =', ndata
   ndata = write_32bit_data_record(achar(0), fd, [1, 2, 3], 3, C_NULL_PTR)
+  print *,'ndata =', ndata
+
+  dims = 0
+  ndim = 0
+  fd = 0
+  pbufr = read_32bit_data_record("./temporary_file"//achar(0), fd, dims, ndim, ndata)
+  if(C_ASSOCIATED(pbufr)) then
+    print *,"pbufr is not NULL"
+    call c_f_pointer(pbufr, rbuf, [dims] )
+    print *,'shape(rbuf) :', shape(rbuf)
+    print *,rbuf
+  else
+    print *,"NULL pointer returned"
+  endif
+  print *,'ndim, dims', ndim, dims
   print *,'ndata =', ndata
 end
