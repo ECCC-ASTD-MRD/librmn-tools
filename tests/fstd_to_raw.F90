c0e7c1adb3889ccaba210f9f354ae856d4df6fc4
M. Valin <mfvalin@gmail.com>
2023-09-06 12:05:27 -0400

    cosmetic readjustments

--- tests/fstd_to_raw.F90
+++ tests/fstd_to_raw.F90
@@ -1,4 +1,4 @@
-program test_compress
+program fstd_to_raw
   use ISO_C_BINDING
   implicit none
   integer, external :: fnom, fstouv, fstnbr, fstinf, fstsui
@@ -13,7 +13,7 @@ program test_compress
   real, dimension(:), pointer :: p=>NULL()
   integer :: sizep
   integer, dimension(:,:), pointer :: iz=>NULL()
-  character (len=128) :: filename, varname, str_ip
+  character (len=128) :: filename, varname, str_ip, dirname
   character (len=128) :: outfile
   integer :: iunout
   integer :: i, j, the_kind, ii
@@ -54,19 +54,37 @@ program test_compress
   real :: ipvalue
   integer(C_SIZE_T) :: nc
   character(len=128) :: c_fname, ipstring
-  integer c1, c2
+  integer c1, c2, c0
 
+  c0 = command_argument_count()
+  if(c0 < 2 .or. c0 > 4) then
+    call get_command_argument(0,filename,ilen,status)
+    print *,'usage : '//trim(filename)//' standard_file variable_name [ dir_name [ out_file_postfix ] ]'
+    stop
+  endif
   write(0,*)'======= converting RPN standard file to raw file ======='
   iun=0
   iunout = 0
   call get_command_argument(1,filename,ilen,status)
   if(status .ne. 0) stop
-  c2 = len(trim(filename))
+  c2 = len(trim(filename))   ! eliminate firs 11 chars of input file for postfix to new file name
   c1 = c2-11
   call get_command_argument(2,varname,ilen,status)
   if(status .ne. 0) stop
+  dirname = 'RAW'
+  if(c0 > 2) then
+    call get_command_argument(3,dirname,ilen,status)
+    if(status .ne. 0) stop
+  endif
 
-  status = fnom(iun,trim(filename),'RND+STD+R/O+OLD',0)
+  status = fnom(iun,trim(filename),'RND+STD+R/O+OLD',0) ! existing std file opened in read-only mode
+  if(c0 > 3) then   ! reuse file name for postfix to new file name
+    call get_command_argument(4,filename,ilen,status)
+    filename = '_'//trim(filename)
+    c1 = 1
+    c2 = len(trim(filename))
+    if(status .ne. 0) stop
+  endif
   call fstopi("MSGLVL",6,0)
   if(status < 0) goto 999
   status = fstouv(iun,'RND')
@@ -90,43 +108,64 @@ program test_compress
                   typvar,nomvar,etiket,grtyp,ig1,ig2,ig3,ig4, &
                   swa,lng,dltf,ubc,extra1,extra2,extra3)
 !       varname(1:2) = nomvar(1:2)
-      if(nomvar(1:2) == varname(1:2) .or. varname(1:1) == '+') then
+      if(nomvar(1:4) == varname(1:4) .or. varname(1:1) == '+') then
         irec = irec + 1
         call fstluk(p,key,ni,nj,nk)
 ! ==========================================================================
+! make clean string from ip1 (no spaces, no starting underscore, no consecutive underscores)
         call CONVIP_plus( ip1, ipvalue, ipkind, -1, ipstring, .true. )  ! convert ip1
         ii = 1
-        do i = 1, 20
+        do i = 1, 20    ! make spaces into underscores
           if(ipstring(i:i) == ' ') then
             ipstring(i:i) = '_'
 !             exit
           endif
         enddo
-        do i = 20, 1, -1
+        do i = 1, 19    ! make consecutive underscores into single underscore
+1         if(ipstring(i:i) == '_' .and. ipstring(i+1:i+1) == '_') then
+            do j = i, 19
+              ipstring(j:j) = ipstring(j+1:j+1)
+            enddo
+            ipstring(20:20) = ' '
+          endif
+          if(ipstring(i:i) == '_' .and. ipstring(i+1:i+1) == '_') goto 1
+        enddo
+        if(ipstring(1:1) == '_')then  ! get rid of underscore at beginning
+          do j = 1, 19
+            ipstring(j:j) = ipstring(j+1:j+1)
+          enddo
+          ipstring(20:20) = ' '
+        endif
+        do i = 20, 1, -1      ! make trailing spaces into underscores
+          if(ipstring(i:i) == ' ') ipstring(i:i) = '_'
+        enddo
+        do i = 20, 1, -1      ! make trailing underscores into spaces
           if(ipstring(i:i) == '_') then
             ipstring(i:i) = ' '
           else
             exit
           endif
         enddo
-        write(c_fname,1111)'RAW/',trim(nomvar),'_'//trim(ipstring)//filename(c1:c2)   ! create file name
-1111 format(A,A,A,I3.3)
+! ==========================================================================
+        write(c_fname,1111)trim(dirname)//'/',trim(nomvar),'_'//trim(ipstring)//filename(c1:c2)   ! create file name
+1111 format(A,A,A)
         fdmode = INT(O'777')
         fd = c_creat(trim(c_fname)//achar(0), fdmode) ! create raw file
         if(fd > 0) then
           ninj = 2         ! 2D data
           nc = 4
-          nc = c_write(fd, C_LOC(ninj), nc)
+          nc = c_write(fd, C_LOC(ninj), nc)    ! number of dimensions (head)
           nc = 4
-          nc = c_write(fd, C_LOC(ni), nc)
+          nc = c_write(fd, C_LOC(ni), nc)      ! first dimension
           nc = 4
-          nc = c_write(fd, C_LOC(nj), nc)
+          nc = c_write(fd, C_LOC(nj), nc)      ! second dimension
           nc = 4 * ni * nj
-          nc = c_write(fd, C_LOC(p(1)), nc)
-          print *,'INFO, wrote',nc+16,' bytes into ',trim(c_fname)
+          nc = c_write(fd, C_LOC(p(1)), nc)    ! data
+          print 2,'INFO, wrote',nc+16," bytes into '",trim(c_fname), "' , avg(p) = ",sum(p)/sizep, ' , avg(|p|) = ',sum(abs(p))/sizep, ', min/max = ', minval(p), maxval(p)
+2         format(A,I10,A,A,A,G10.5,A,G10.5,A,2G10.5)
           ninj = ni * nj ;
           nc = 4
-          nc = c_write(fd, C_LOC(ninj), nc)
+          nc = c_write(fd, C_LOC(ninj), nc)    ! number of dimensions (tail)
           fdstatus = c_close(fd)
         else
           print *,'ERROR creating '//trim(c_fname)
