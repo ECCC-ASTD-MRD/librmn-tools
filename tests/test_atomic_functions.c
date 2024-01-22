b50fafda3f0443dced8a78ea86bfba715e920d9e
M. Valin <mfvalin@gmail.com>
2024-01-22 17:21:53 -0500

    more thread functions in test

--- tests/test_atomic_functions.c
+++ tests/test_atomic_functions.c
@@ -14,6 +14,7 @@
 // Author:
 //     M. Valin,   Recherche en Prevision Numerique, 2024
 //
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -22,21 +23,110 @@
 // RPN atomic functions
 #include <rmn/atomic_functions.h>
 
-#if !defined(NTHREADS)
-#define NTHREADS 12
+#include <errno.h>
+#define handle_error_en(en, msg) do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
+
+#if !defined(MAX_THREADS)
+#define MAX_THREADS 12
 #endif
 #define NREP 10000
 
 static  _Atomic int acnt;
 static  int xcnt, dummy;
-static  int32_t bcnt = NREP*NTHREADS ;
-static  int32_t pcnt = NREP*NTHREADS ;
-static  pthread_t tid[NTHREADS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+static  int32_t bcnt = NREP*MAX_THREADS ;
+static  int32_t pcnt = NREP*MAX_THREADS ;
+// static  pthread_t tid[MAX_THREADS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+static  pthread_t tid[MAX_THREADS+1] = {[0 ... MAX_THREADS] = 0xFFFFFFFFFFFFFFFFu};
 static  void *arguments = &pcnt;                  // argument list for called thread
+static  pthread_attr_t attr;
 
-int thread_index(pthread_t thread_id){
+int pthread_getattr_np(pthread_t thread, pthread_attr_t *attr); // in case it is missing
+
+static void thread_display_attributes(int my_thread_no, char *prefix)
+{
+  int s, i;
+  size_t v;
+  void *stkaddr;
+  struct sched_param sp;
+  pthread_attr_t gattr;
+  pthread_attr_t *attr = &gattr;
+
+  printf("thread %d\n", my_thread_no);
+
+  s = pthread_getattr_np(tid[my_thread_no], attr);
+  if (s != 0)
+    handle_error_en(s, "pthread_getattr_np");
+
+  s = pthread_attr_getdetachstate(attr, &i);
+  if (s != 0)
+      handle_error_en(s, "pthread_attr_getdetachstate");
+  printf("%sDetach state        = %s\n", prefix,
+          (i == PTHREAD_CREATE_DETACHED) ? "PTHREAD_CREATE_DETACHED" :
+          (i == PTHREAD_CREATE_JOINABLE) ? "PTHREAD_CREATE_JOINABLE" :
+          "???");
+
+  s = pthread_attr_getscope(attr, &i);
+  if (s != 0)
+      handle_error_en(s, "pthread_attr_getscope");
+  printf("%sScope               = %s\n", prefix,
+          (i == PTHREAD_SCOPE_SYSTEM)  ? "PTHREAD_SCOPE_SYSTEM" :
+          (i == PTHREAD_SCOPE_PROCESS) ? "PTHREAD_SCOPE_PROCESS" :
+          "???");
+
+  s = pthread_attr_getinheritsched(attr, &i);
+  if (s != 0)
+      handle_error_en(s, "pthread_attr_getinheritsched");
+  printf("%sInherit scheduler   = %s\n", prefix,
+          (i == PTHREAD_INHERIT_SCHED)  ? "PTHREAD_INHERIT_SCHED" :
+          (i == PTHREAD_EXPLICIT_SCHED) ? "PTHREAD_EXPLICIT_SCHED" :
+          "???");
+
+  s = pthread_attr_getschedpolicy(attr, &i);
+  if (s != 0)
+      handle_error_en(s, "pthread_attr_getschedpolicy");
+  printf("%sScheduling policy   = %s\n", prefix,
+          (i == SCHED_OTHER) ? "SCHED_OTHER" :
+          (i == SCHED_FIFO)  ? "SCHED_FIFO" :
+          (i == SCHED_RR)    ? "SCHED_RR" :
+          "???");
+
+  s = pthread_attr_getschedparam(attr, &sp);
+  if (s != 0)
+      handle_error_en(s, "pthread_attr_getschedparam");
+  printf("%sScheduling priority = %d\n", prefix, sp.sched_priority);
+
+  s = pthread_attr_getguardsize(attr, &v);
+  if (s != 0)
+      handle_error_en(s, "pthread_attr_getguardsize");
+  printf("%sGuard size          = %zu bytes\n", prefix, v);
+
+  s = pthread_attr_getstack(attr, &stkaddr, &v);
+  if (s != 0)
+      handle_error_en(s, "pthread_attr_getstack");
+  printf("%sStack address       = %p\n", prefix, stkaddr);
+  printf("%sStack size          = %ld bytes\n", prefix, v);
+//   printf("%sStack size          = 0x%zx bytes\n", prefix, v);
+}
+
+// next free slot in thread table
+int thread_get_free_slot(){
   int i;
-  for(i=0 ; i<NTHREADS ; i++) if(thread_id == tid[i]) return i;
+  for(i=0 ; i<MAX_THREADS ; i++){
+    if( atomic_compare_and_swap_64(&tid[i], 0xFFFFFFFFFFFFFFFFu, 0) ) return i ;
+  }
+  return -1;
+}
+
+int thread_my_slot(){
+  int i;
+  pthread_t thread_id = pthread_self() ;
+  for(i=0 ; i<MAX_THREADS ; i++) if(thread_id == tid[i]) return i;
+  return -1 ;
+}
+
+int thread_get_slot(pthread_t thread_id){
+  int i;
+  for(i=0 ; i<MAX_THREADS ; i++) if(thread_id == tid[i]) return i;
   return -1 ;
 }
 
@@ -45,20 +135,31 @@ void *adding(void *in)
   int32_t *input = (int32_t *) in;
   int status = -1, bcnt_, i, status2 = -1 ;
   int64_t nwait ;
-  pthread_t self = pthread_self();
-  int thread_no = thread_index(self);
+  int thread_no = thread_my_slot();
+  pthread_t self = tid[thread_no] ;
+  pthread_t self1 = pthread_self();    // for consistency check
   int startvalue = bcnt;
+  int next_thread = thread_get_free_slot() ;
+  size_t mystacksize;
+  pthread_attr_t gattr;
 
-  // launch next thread unless enough extra threads (NTHREADS-1) have been launched
-  if(thread_no < NTHREADS-1){
+  if(self != self1) exit(1) ;
+
+//   fprintf(stderr,"thread %2d, next free slot = %2d\n", thread_no, next_thread);
+  thread_display_attributes(thread_no, "\t");
+
+  // launch next thread unless enough extra threads (MAX_THREADS-1) have been launched
+  if(next_thread != -1) {
 //     fprintf(stderr,"thread %2d launching thread %2d\n",thread_no, thread_no+1);
-    pthread_create(&tid[thread_no+1],NULL,adding,arguments);
+    pthread_create(&tid[next_thread], &attr, adding, arguments);
+//     pthread_create(&tid[next_thread], NULL, adding, arguments);
+//     pthread_create(&tid[thread_no+1],NULL,adding,arguments);
   }else if(thread_no < 0){
-    fprintf(stderr,"ERROR: bad thread ordinal = %2d (thread id = %ld)\n", thread_no, self);
+    fprintf(stderr,"ERROR: bad thread ordinal = %2d (thread id = %ld %ld)\n", thread_no, self, self1);
     exit(1);
   }
 
-  for(i=0 ; i<(NTHREADS-thread_no)*5000 ; i++)
+  for(i=0 ; i<(MAX_THREADS-thread_no)*5000 ; i++)
     atomic_add_and_test_32(&dummy, i);            // spin for some time (earlier threads spin more)
   for(i=0; i<NREP; i++)
   {
@@ -78,20 +179,27 @@ void *adding(void *in)
 int main(int argc, char **argv)
 {
   int i, success;
+//   size_t stacksize = 2048l << 20;   // 2GBytes
+  size_t stacksize = 2000l * 1000000;   // 2GBytes
+//   size_t stacksize = 2 << 10;   // 2KBytes
 
+  pthread_attr_init(&attr);
+  pthread_attr_setstacksize(&attr, stacksize) ;
+
+//   fprintf(stderr,"size of pthread_t = %ld\n", sizeof(pthread_t));
   tid[0] = pthread_self() ;                 // thread 0 is the main thread
   adding(arguments);                        // thread 0 will get extra threads launched
 
-  for(i=1; i<NTHREADS; i++)                 // wait for extra threads to terminate
+  for(i=1; i<MAX_THREADS; i++)                 // wait for extra threads to terminate
     pthread_join(tid[i],NULL);
 
-  printf("the value of acnt is %7d, expecting %d\n", acnt, NREP*NTHREADS);
+  printf("the value of acnt is %7d, expecting %d\n", acnt, NREP*MAX_THREADS);
   printf("the value of bcnt is %7d, expecting %d\n", bcnt,             0);
   printf("the value of pcnt is %7d, expecting %d\n", pcnt,             0);
-  printf("the value of xcnt is %7d, expecting %d\n", xcnt, NREP*NTHREADS);
+  printf("the value of xcnt is %7d, expecting %d\n", xcnt, NREP*MAX_THREADS);
   printf("the value of dummy is %8.8x\n", dummy);
 
-  success = (acnt == NREP*NTHREADS && bcnt == 0 && pcnt == 0);
+  success = (acnt == NREP*MAX_THREADS && bcnt == 0 && pcnt == 0);
   fprintf(stderr,"%s\n", success ? "SUCCESS" : "FAILURE") ;
   return (success ? 0 : 1);
 }
