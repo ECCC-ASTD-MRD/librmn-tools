a9891c08669153dae606a66e4116761e5e63f1ee
M. Valin <mfvalin@gmail.com>
2024-07-07 12:21:18 -0400

    faster insert for nbits < 8

--- src/pack/bi_endian_pack.c
+++ src/pack/bi_endian_pack.c
@@ -24,6 +24,10 @@
 #include <rmn/bits.h>
 #define STATIC extern
 #include <rmn/bi_endian_pack.h>
+// word stream macros and functions
+// #include <rmn/word_stream.h>
+// bit stream macros and functions
+#include <rmn/bit_stream.h>
 
 // little endian style insertion of values into a bit stream
 // p     : stream                                [INOUT]
@@ -37,12 +41,20 @@ int  LeStreamInsert(bitstream *p, uint32_t *w32, int nbits, int nw){
   if(! STREAM_INSERT_MODE(*p) )      return 0;   // ERROR: not in insert mode
   if(! STREAM_IS_LITTLE_ENDIAN(*p) ) return 0 ;  // wrong endianness
 
+  if(nbits <= 8) {                              // process values two at a time
+    uint32_t t, mask = RMASK32(nbits), nb = nbits + nbits ;
+    for(    ; i<n-3 ; i+=4){
+      // little endian =>  lower part [i] | upper part [i+1]
+      t  = (w32[i  ] & mask) | ((w32[i+1] & mask) << nbits) | ((w32[i+2] & mask) << nb) | ((w32[i+3] & mask) << (nb+nbits)) ;
+      LE64_EZ_FAST_PUT_NBITS(t, nb+nb) ;                 // insert a pair of values
+    }
+  }
   if(nbits <= 16) {                              // process values two at a time
     uint32_t t, mask = RMASK32(nbits), nb = nbits + nbits ;
     for(    ; i<n-1 ; i+=2){
       // little endian =>  lower part [i] | upper part [i+1]
       t  =              (w32[i  ] & mask) | ((w32[i+1] & mask) << nbits) ;
-      LE64_EZ_PUT_NBITS(t, nb) ;                 // insert a pair of values
+      LE64_EZ_FAST_PUT_NBITS(t, nb) ;                 // insert a pair of values
     }
   }
   for(    ; i<n ; i++){
@@ -69,12 +81,21 @@ int  BeStreamInsert(bitstream *p, uint32_t *w32, int nbits, int nw){
   if(! STREAM_INSERT_MODE(*p) )   return 0;      // ERROR: not in insert mode
   if(! STREAM_IS_BIG_ENDIAN(*p) ) return 0 ;     // wrong endianness
 
+  uint32_t mask = RMASK32(nbits) ;
+  if(nbits <= 8) {       // process values four at a time
+    uint32_t t, nb = nbits + nbits ;
+    for(    ; i<n-3 ; i+=4){
+      // big endian => lower part [i+1] | upper part [i]
+      t  = (w32[i+3] & mask) | ((w32[i+2] & mask) << nbits) | ((w32[i+1] & mask) << nb) | ((w32[i+0] & mask) << (nb+nbits)) ;
+      BE64_EZ_FAST_PUT_NBITS(t, nb+nb) ;                 // insert a pair of values
+    }
+  }
   if(nbits <= 16) {       // process values two at a time
-    uint32_t t, mask = RMASK32(nbits), nb = nbits + nbits ;
+    uint32_t t, nb = nbits + nbits ;
     for(    ; i<n-1 ; i+=2){
       // big endian => lower part [i+1] | upper part [i]
       t  =            (w32[i+1] & mask) | ((w32[i  ] & mask) << nbits) ;
-      BE64_EZ_PUT_NBITS(t, nb) ;                 // insert a pair of values
+      BE64_EZ_FAST_PUT_NBITS(t, nb) ;                 // insert a pair of values
     }
   }
   for(    ; i<n ; i++){
