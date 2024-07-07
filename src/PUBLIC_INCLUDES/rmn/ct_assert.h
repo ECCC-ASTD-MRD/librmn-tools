d5ccee0f0f76441c6383965a9e7f3f3628bc7d92
M. Valin <mfvalin@gmail.com>
2024-07-07 14:41:52 -0400

    inserted license

--- src/PUBLIC_INCLUDES/rmn/ct_assert.h
+++ src/PUBLIC_INCLUDES/rmn/ct_assert.h
@@ -1,10 +1,26 @@
-
+//
+// Copyright (C) 2022-2024  Environnement Canada
+//
+// This is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation,
+// version 2.1 of the License.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details .
+//
+// M. Valin,   Recherche en Prevision Numerique, 2024
+//
+// static assertion macros
+// CT_ASSERT  with message
+// CT_ASSERT_ without message
 #if ! defined(CT_ASSERT)
 
 #if (__STDC_VERSION__ >= 201112L)  // C11+
 
 #define CT_ASSERT(e, message) _Static_assert(e, message) ;
-// #define CT_ASSERT_(e) _Static_assert(e, "") ;
 
 #else
 
