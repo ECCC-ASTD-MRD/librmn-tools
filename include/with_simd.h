99c375477e8c59179d49c98dba42aa7dd3fafcb3
M. Valin <mfvalin@gmail.com>
2024-08-26 10:22:53 -0400

    define macro name change, slightly different logic

--- include/with_simd.h
+++ include/with_simd.h
@@ -1,24 +1,24 @@
 // control usage of Intel SIMD intrinsics
-// by default SIMD intrinsics and assembler code are used on x86_64 and aarch64
-// use -DNO_SIMD to refrain from using SIMD intrinsics on x86_64 architectures
+// by default SIMD intrinsics and assembler code are used on x86_64 architectures
+// use -DWITHOUT_SIMD to refrain from using Intel SIMD intrinsics on x86_64 architectures
 #if ! defined(WITH_SIMD_CONTROL_DONE)
 #define WITH_SIMD_CONTROL_DONE
 
-#if ! defined(NO_SIMD)
+#if ! defined(WIHOUT_SIMD)
 #define WITH_SIMD
 
-#if defined(__x86_64__) && defined(__AVX2__)
+// Intel SIMD intrinsics
+#if defined(__x86_64__)
+#if defined(__AVX2__) || defined(__SSE2__)
 #include <immintrin.h>
 #endif
-#if defined(__x86_64__) && defined(__SSE2__)
-#include <emmintrin.h>
 #endif
 
 #if ! defined(QUIET_SIMD)
 // #pragma message("NOTE: using SIMD intrinsics, use -DNO_SIMD to use pure C code")
 #warning "NOTE: using Intel SIMD intrinsics, use -DNO_SIMD to use pure C code"
-#endif
+#endif   // QUIET_SIMD
 
-#endif
+#endif   // WIHOUT_SIMD
 
-#endif
+#endif   // WITH_SIMD_CONTROL_DONE
