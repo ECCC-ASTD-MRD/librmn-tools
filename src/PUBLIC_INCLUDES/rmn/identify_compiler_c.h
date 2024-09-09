07326e08b1e5bc5f60af07044d4949024d08fc87
M. Valin <mfvalin@gmail.com>
2024-09-09 12:02:34 -0400

    fixed warnings

--- src/PUBLIC_INCLUDES/rmn/identify_compiler_c.h
+++ src/PUBLIC_INCLUDES/rmn/identify_compiler_c.h
@@ -20,45 +20,54 @@
 #define IDENTIFY_C_COMPILER
 
 #if defined(__x86_64__)
-static int ADDRESS_MODE = 64 ;
+// static int ADDRESS_MODE = 64 ;
+#define ADDRESS_MODE 64
 
 #elif defined(__i386__)
 static int ADDRESS_MODE = 32 ;
+#define ADDRESS_MODE 32
 
 #else
-static int ADDRESS_MODE = -1 ;
+// static int ADDRESS_MODE = -1 ;
+#define ADDRESS_MODE -1
 
 #endif
 
 #if defined(__INTEL_LLVM_COMPILER)
 // #warning "ICX detected"
 #define COMPILER_IS_ICX
-  static char C_COMPILER[] = "INTEL_ICX" ;  // icx
+#define C_COMPILER "INTEL_ICX"
+//   static char C_COMPILER[] = "INTEL_ICX" ;  // icx
 
 #elif defined(__INTEL_COMPILER)
 // #warning "ICC detected"
 #define COMPILER_IS_ICC
-  static char C_COMPILER[] = "INTEL_ICC" ;  // icc
+#define C_COMPILER "INTEL_ICC"
+//   static char C_COMPILER[] = "INTEL_ICC" ;  // icc
 
 #elif defined(__PGI)
 // #warning "PGI detected"
 #define COMPILER_IS_PGI
-  static char C_COMPILER[] = "PGI/Nvidia" ;   // pgcc nvcc
+#define C_COMPILER "PGI/Nvidia"
+//   static char C_COMPILER[] = "PGI/Nvidia" ;   // pgcc nvcc
 
 #elif defined(__clang__)
 // #warning "CLANG detected"
 #define COMPILER_IS_CLANG
-  static char C_COMPILER[] = "CLANG" ;        // llvm/aocc clang
+#define C_COMPILER "CLANG"
+//   static char C_COMPILER[] = "CLANG" ;        // llvm/aocc clang
 
 #elif defined(__GNUC__)
 // #warning "GNU detected"
 #define COMPILER_IS_GCC
-  static char C_COMPILER[] = "GNU" ;          // gcc or lookalike
+#define C_COMPILER "GCC"
+//   static char C_COMPILER[] = "GNU" ;          // gcc or lookalike
 
 #else
 // #warning "UNKNOWN detected"
 #define COMPILER_IS_UNKNOWN
-  static char C_COMPILER[] = "UNKNOWN" ;      // unknown
+#define C_COMPILER "UNKNOWN"
+//   static char C_COMPILER[] = "UNKNOWN" ;      // unknown
 
 #endif
 
--- tests/test_identify_compiler_c.c
+++ tests/test_identify_compiler_c.c
@@ -6,9 +6,11 @@
 char *identify_compiler() ;
 int identify_address_mode() ;
 
-int main(int argc, char **argv){
+// int main(int argc, char **argv){
+int main(){
   start_of_test("identify_compiler_c");
   fprintf(stderr, "compiler = '%s', address mode = %d bits\n", C_COMPILER, ADDRESS_MODE) ;
   fprintf(stderr, "identify_compiler() = '%s'\n", identify_compiler()) ;
   fprintf(stderr, "identify_address_mode() = %d\n", identify_address_mode()) ;
+  return 0 ;
 }
