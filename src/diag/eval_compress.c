a27357db6fe2ac703d910f7e899b6d3d5b5e430d
M. Valin <mfvalin@gmail.com>
2025-01-15 11:34:55 -0500

    saving work

--- src/diag/eval_compress.c
+++ src/diag/eval_compress.c
@@ -12,9 +12,12 @@
 // Library General Public License for more details.
 //
 #include<stdio.h>
+#include<stdint.h>
+#include<stdlib.h>
 
 #include <rmn/eval_compress.h>
 
+// get a smaller contiguous block from large array src
 static void get_block(int lni, int lnj, int i0, int j0, int src[lnj][lni], int ni, int nj, int dst[nj][ni]){
   int i, j ;
   for(j=0 ; j<nj ; j++){
@@ -24,25 +27,69 @@ static void get_block(int lni, int lnj, int i0, int j0, int src[lnj][lni], int n
   }
 }
 
+// return number of bits needed to represent n (assumed >= 0)
+static int bits_needed(int n){
+  int nbits = 0 ;
+  while(n > 0){
+    nbits++ ;
+    n >>= 1 ;
+  }
+  return nbits ;
+}
+
+// get min and max of integer array
+static void get_min_max_i(int *buf, int ninj, int *min, int *max){
+  int i ;
+  int mi = buf[0], ma = buf[0];
+  for(i=1 ; i<ninj ; i++){
+    mi = (buf[i] < mi) ? buf[i] : mi ;
+    ma = (buf[i] > ma) ? buf[i] : ma ;
+  }
+  *min = mi ;
+  *max = ma ;
+}
+
+// print block
+static void print_block(int ni, int nj, int block[nj][ni]){
+  int i, j ;
+  for(j=nj-1 ; j>=0 ; j--){
+    for(i=0 ; i<ni ; i++){
+      fprintf(stderr, "%10d", block[j][i]);
+    }
+    fprintf(stderr, "\n");
+  }
+  fprintf(stderr, "\n");
+}
+
+// return estimate of number of bits needed to encode block
 static int count_bits(int ni, int nj, int block[nj][ni]){
-  int i, j, max, min, nbits, range, range0 ;
+  int i, j, max, min, nbits, range, extra ;
   max = min = block[0][0] ;
-  for(j=0 ; j<nj ; j++){
+  for(j=nj-1 ; j>=0 ; j--){
     for(i=0 ; i<ni ; i++){
       max = (block[j][i] > max) ? block[j][i] : max ;
       min = (block[j][i] < min) ? block[j][i] : min ;
     }
   }
-  range0 = range = max - min ;
-  nbits = 0 ;
-  while(range > 0){
-    nbits++ ;
-    range >>= 1 ;
+//   range = max - min ;
+//   extra = 21 ;
+  if(max * min < 0){   // positive and negative numbers are present
+    min = -min ;       // abs(min)
+    max = (min > max) ? min : max ;   // largest absolute value between min and max
+    range = 2 * max ;                 // zigzag coding
+    extra = 0 ;
+  }else{               // all numbers have the same sign, use an offset
+    range = max - min ;
+    if(min < range/4 && min > 0) range = max ;
+    extra = 21 ;       // offset + nbits
   }
-// fprintf(stderr, "min = %6d, max=%6d, range = %6d, nbits = %6d\n", min, max, range0, nbits) ;
-  return ni * nj * nbits ;
+  nbits = bits_needed(range) ;
+//   int range0 = range ;
+// if(ni*nj > 1000000) fprintf(stderr, "count_bits : min = %d, max = %d, range = %d, nbits = %d\n", min, max, range0, nbits) ;
+  return ni * nj * nbits + extra ;
 }
 
+// apply 2D Lorenzo predictor to block, result in pred
 static void lorenzo(int ni, int nj, int block[nj][ni], int pred[nj][ni]){
   int i, j ;
   pred[0][0] = block[0][0] ;                                        // lower left corner as is
@@ -53,26 +100,73 @@ static void lorenzo(int ni, int nj, int block[nj][ni], int pred[nj][ni]){
   }
 }
 
+// return power of 2 <= err
+static float power2_err(float err){
+  union{
+    uint32_t u ;
+    float    f ;
+  } uf ;
+  uf.f = err ;
+  uf.u &= 0xFF800000u ;
+  return uf.f ;
+}
+
+// return estimate of number of bits necessary to quantize and encode float array f with prediction
+// bsize   [IN]: dimension of quantization/prediction blocks
+// errmax  [IN]: quantization interval (power of 2 <= errmax will be used)
+// btab   [OUT]: more detailed information
 int float_compressed_bits(int ni, int nj, float f[nj][ni], float errmax, int *btab, int bsize){
+  int q[nj][ni] ;
+  int p[nj][ni] ;
   int block[bsize*bsize] ;
   int pred[bsize*bsize] ;
   int block8[8*8] ;
-  int nbits = 0, nblocks = 0, nblock8 = 0, nbits64 = 0, npred = 0, nbits8 = 0 , npred8 = 0 ;
-  int i0, j0, i, j, in, jn, ix, i8, j8 ;
+  int nbits = 0, nblocks = 0, nblock8 = 0, nbits64 = 0, npred = 0, nbits8 = 0 , npred8 = 0, nbitsg = 0, nbitsp = 0, asym = 0, rawp8 = 0, nraw8 = 0 ;
+  int i0, j0, i, j, in, jn, ix, i8, j8, min, max ;
+
+  errmax = power2_err(errmax) ;  // power of 2 <= errmax
+  // quantize the whole array
+  for(j=0 ; j<nj ; j++){
+    for(i=0 ; i<ni ; i++){
+      q[j][i] = f[j][i] / errmax + .5f ;
+    }
+  }
+  get_min_max_i((void *)q, ni*nj, &min, &max);
+  nbitsg = 64 + count_bits(ni, nj, (void *)q) ;  // nbits for the whole array
+  // predict the whole array
+  lorenzo(ni, nj, (void *)q, (void *)p) ;
+  p[0][0] = 0 ;
+  nbitsp = 96 + count_bits(ni, nj, (void *)p) ;  // nbits for the whole predicted array
+  rawp8 = 96 ;
+  // subdivide the whole predicted array into 8x8 blocks
+  for(j8=0 ; j8<nj ; j8+=8){
+    int j8n = ((j8+8) > nj) ? (nj - j8) : 8 ;
+    for(i8=0 ; i8<ni ; i8+=8){
+      nraw8++ ;
+      int i8n = ((i8+8) > ni) ? (ni - i8) : 8 ;
+      rawp8 += 20 ; // average encoding block overhead
+      get_block(ni, nj, i8, j8, (void *)p, i8n, j8n, (void *)block8) ;
+// if(i8 == 512 && j8 == 512) print_block(i8n, j8n, (void *)block8) ;
+      rawp8 += count_bits(i8n, j8n, (void *)block8) ;
+    }
+  }
+
   for(j0=0 ; j0<nj ; j0+=bsize){
     jn = ((j0 + bsize) > nj) ? (nj - j0) : bsize ;
     for(i0=0 ; i0<ni ; i0+=bsize){
-      nblocks++ ;    // count large blocks
+      nblocks++ ;    // count quantization/prediction blocks
       in = ((i0 + bsize) > ni) ? (ni - i0) : bsize ;
       ix = 0 ;
       nbits += 64 ; // large block overhead
-      // quantize
+      // get small quantized block
       for(j=0 ; j<jn ; j++){
         for(i=0 ; i<in ; i++){
-          block[ix] = f[j0+j][i0+i] / errmax + .5f ;
+//           block[ix] = f[j0+j][i0+i] / errmax + .5f ;
+          block[ix] = q[j0+j][i0+i] ;
           ix++ ;
         }
       }
+      // bits needed if not subdividing quantization block
       nbits64 = nbits64 + 64 + count_bits(in, jn, (void *)block) ;
       // subdivide large block into 8 x 8 encoding blocks, count bits
       nbits8 += 64 ;
@@ -80,36 +174,48 @@ int float_compressed_bits(int ni, int nj, float f[nj][ni], float errmax, int *bt
         int j8n = ((j8+8) > jn) ? (jn - j8) : 8 ;
         for(i8=0 ; i8<in ; i8+=8){
           int i8n = ((i8+8) > in) ? (in - i8) : 8 ;
-          nbits8 += 16 ; // encoding block overhead
+          nbits8 += 20 ; // average encoding block overhead
           get_block(in, jn, i8, j8, (void *)block, i8n, j8n, (void *)block8) ;
+// if(i0 == 512 && j0 == 512 && j8 == 0 && i8 == 0) print_block(i8n, j8n, (void *)block8) ;
           nbits8 += count_bits(i8n, j8n, (void *)block8) ;
         }
       }
-      // apply predictor
+      // apply predictor to quantization block
       lorenzo(in, jn, (void *)block, (void *)pred) ;
       pred[0] = 0 ;
+      // bits needed if not subdividing predicted block
       npred = npred + 64 + 32 + count_bits(in, jn, (void *)pred) ;
       npred8 = npred8 + 64 + 32 ;  // large block overhead
-      // subdivide predicted large blocks into 8 x 8 encoding blocks, count bits
+      // subdivide predicted blocks into 8 x 8 encoding blocks, count bits
       // TODO collect distribution of nbits
       for(j8=0 ; j8<jn ; j8+=8){
         int j8n = ((j8+8) > jn) ? (jn - j8) : 8 ;
         for(i8=0 ; i8<in ; i8+=8){
           nblock8++ ;   // count encoding blocks
           int i8n = ((i8+8) > in) ? (in - i8) : 8 ;
-          npred8 += 16 ; // encoding block overhead
+          npred8 += 20 ; // average encoding block overhead
           get_block(in, jn, i8, j8, (void *)pred, i8n, j8n, (void *)block8) ;
-          npred8 += count_bits(i8n, j8n, (void *)block8) ;
+// if(i0 == 512 && j0 == 512 && j8 == 0 && i8 == 0) print_block(i8n, j8n, (void *)block8) ;
+          int nbi = count_bits(i8n, j8n, (void *)block8) ;
+          npred8 += nbi ;
+          get_min_max_i((void *)block8, i8n*j8n, &min, &max);
+          if(bits_needed(max-min)*i8n*j8n > nbi) asym++ ;
         }
       }
     }
   }
+  if(nraw8 != nblock8) exit(1);
+  // detailed stats
   btab[0] = nblocks ;   // number of quantization/prediction blocks
   btab[1] = nblock8 ;   // number of encoding blocks
   btab[2] = nbits64 ;   // number of bits for quantized only blocks
   btab[3] = nbits8 ;    // number of bits for quantized encoded blocks
   btab[4] = npred ;     // number of bits for quantized predicted blocks
   btab[5] = npred8 ;    // number of bits for quantized predicted encoded blocks
+  btab[6] = nbitsg ;    // number of bits for global quantized array
+  btab[7] = nbitsp ;    // number of bits for global predicted array
+  btab[8] = asym ;      // number of "asymmetric" blocks
+  btab[9] = rawp8 ;     // number of bits for quantized encoded 8x8 global array
 //   fprintf(stderr, "float_compressed_bits: %d large blocks, %d encoding blocks, nbits64 = %d, nbits8 = %d, npred = %d, npred8 = %d\n",
 //                   nblocks, nblock8, nbits64, nbits8, npred, npred8) ;
   return npred8 ;
--- tests/test_eval_compress.c
+++ tests/test_eval_compress.c
@@ -30,16 +30,39 @@ typedef union{
 #define ABS(X) (((X)>0) ? (X) : -(X))
 #define XOR 0x7
 
+static float power2_err(float err){
+  union{
+    uint32_t u ;
+    float    f ;
+  } uf ;
+  uf.f = err ;
+  uf.u &= 0xFF800000u ;
+  return uf.f ;
+}
+
+void get_min_max(float *buf, int ninj, float *min, float *max){
+  int i ;
+  float mi = buf[0], ma = buf[0];
+  for(i=1 ; i<ninj ; i++){
+    mi = (buf[i] < mi) ? buf[i] : mi ;
+    ma = (buf[i] > ma) ? buf[i] : ma ;
+  }
+  *min = mi ;
+  *max = ma ;
+}
+
 int main(int argc, char **argv){
   float f[NJ][NI], bpp, errmax = .125f, min, max, npts ;
-  uf f1, f2, f3, n1, n2, n3 ;
   char *filename = NULL ;
   int i, j, nbits, fd = 0, ndim = 0, ndata, nij ;
   int dims[10], btab[10] ;
   void *buf ;
 
   fprintf(stderr, "================= test eval_compress ================\n") ;
+#if 0
   fprintf(stderr, "================= float resolution ==================\n") ;
+  uf f1, f2, f3, n1, n2, n3 ;
+
   f1.f=1.0f ; f2.f=2.0f ; f3.f=5.0f ;
   for(i=0 ; i<5 ; i++){
     n1.f = f1.f ; n2.f = f2.f ; n3.f = f3.f ;
@@ -47,29 +70,36 @@ int main(int argc, char **argv){
     fprintf(stderr, "%7.1f(%9.7f) %7.1f(%9.7f) %7.1f(%9.7f)\n", f1.f, ABS(f1.f-n1.f), f2.f, ABS(f2.f-n2.f), f3.f, ABS(f3.f-n3.f)) ;
     f1.f *= 10 ; f2.f *= 10 ; f3.f *= 10 ;
   }
-  fprintf(stderr, "================= original data =====================\n") ;
+#endif
   if(argc > 1) errmax = atof(argv[1]) ;
-  if(argc > 2) {
-    filename = argv[2] ;
-    buf = read_32bit_data_record(filename, &fd, dims, &ndim, &ndata) ;
-    while(buf != NULL){
-      fprintf(stderr, "number of dimensions = %d : (", ndim) ;
-      for(j=0 ; j<ndim ; j++) { fprintf(stderr, "%d ", dims[j]) ; } ;
-      fprintf(stderr, ") [%d]\n", ndata);
-      nij = dims[0]*dims[1] ; npts = nij ;
-      nbits = float_compressed_bits(dims[0], dims[1], (void *)buf, errmax, btab, 64);
-      bpp = nbits ; bpp = bpp / nij ;
-      fprintf(stderr, "with prediction : error = %5.2f, nbits = %d (%5.2f bits/value)\n", errmax, nbits, bpp) ;
-      fprintf(stderr, "float_compressed_bits: %d large blocks, %d encoding blocks, nbits64 = %d, nbits8 = %d, npred = %d, npred8 = %d\n",
-                      btab[0], btab[1], btab[2], btab[3], btab[4], btab[5]) ;
-      fprintf(stderr, "float_compressed_bits: block64 = %5.2f bits/value, block8 = %5.2f bits/value, pred64 = %5.2f bits/value, pred8 = %5.2f bits/value\n",
-                      btab[2]/npts, btab[3]/npts, btab[4]/npts, btab[5]/npts) ;
+  errmax = power2_err(errmax) ;
+  if(argc < 3) goto synthetic ;
+  filename = argv[2] ;
 
-      free(buf) ;
-      buf = read_32bit_data_record(filename, &fd, dims, &ndim, &ndata) ;
-    }
+  // ndim == number of dimensions, dims[] == dimensions, ndata == number of data elements, fd == file descriptor
+  buf = read_32bit_data_record(filename, &fd, dims, &ndim, &ndata) ;  // get data record
+  while(buf != NULL){
+    fprintf(stderr, "\nnumber of dimensions = %d : (", ndim) ;
+    for(j=0 ; j<ndim ; j++) { fprintf(stderr, "%d ", dims[j]) ; } ;
+    fprintf(stderr, ") [%d]", ndata);
+    nij = dims[0]*dims[1] ; npts = nij ;
+    get_min_max(buf, nij, &min, &max) ;
+    fprintf(stderr, ", min = %10G, max = %10G, range = %10G, Q = %G, f16(Q = %G), f12(Q = %G)\n", min, max, max-min, errmax, (max-min)/65536, (max-min)/4096) ;
+    nbits = float_compressed_bits(dims[0], dims[1], (void *)buf, errmax, btab, 64);
+    bpp = nbits ; bpp = bpp / npts ;
+    fprintf(stderr, "compressed_bits : %d blocks, %d encoding blocks, quant = %d, quant64 = %d, quant64-8 = %d, pred64 = %d, pred64-8 = %d\n",
+                    btab[0], btab[1], btab[6], btab[2], btab[3], btab[4], btab[5]) ;
+    fprintf(stderr, "bits/value      : quant = %5.2f, pred =%5.2f, pred-8 =%5.2f, quant64 = %5.2f, quant64-8 = %5.2f, pred64 = %5.2f, pred64-8 = %5.2f\n",
+                    btab[6]/npts, btab[7]/npts, btab[9]/npts, btab[2]/npts, btab[3]/npts, btab[4]/npts, btab[5]/npts) ;
+
+    free(buf) ;
+
+    buf = read_32bit_data_record(filename, &fd, dims, &ndim, &ndata) ;   // try to get next data record
   }
-return 0 ;
+  return 0 ;
+
+  fprintf(stderr, "================= synthetic data =====================\n") ;
+synthetic:     // in case no filename is given
   min = 999999.0 ;
   max = -min ;
   for(j=0 ; j<NJ ; j++){
@@ -80,16 +110,12 @@ return 0 ;
     }
   }
   fprintf(stderr, "min = %12G, max = %12G, max error = %12G\n", min, max, errmax) ;
-  fprintf(stderr, "================= without predictor =================\n") ;
-  nbits = float_compressed_bits(NI, NJ, (void *)f, errmax, btab, 64);
-  bpp = nbits ;
-  bpp = bpp / (NI*NJ) ;
-  fprintf(stderr, " error = %5.2f, nbits = %d (%5.2f bits/value)\n", errmax, nbits, bpp) ;
 
   fprintf(stderr, "================= with predictor ====================\n") ;
   nbits = float_compressed_bits(NI, NJ, (void *)f, errmax, btab, 64);
   bpp = nbits ;
   bpp = bpp / (NI*NJ) ;
   fprintf(stderr, " error = %5.2f, nbits = %d (%5.2f bits/value)\n", errmax, nbits, bpp) ;
+  return 0 ;
 }
 
