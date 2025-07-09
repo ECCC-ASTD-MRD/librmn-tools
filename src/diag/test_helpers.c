322cd8fadc547d7198ce587d09de78df0c8d3102
M. Valin <mfvalin@gmail.com>
2025-07-09 12:03:12 -0400

    code moved to test_helpers.h

--- src/diag/test_helpers.c
+++ src/diag/test_helpers.c
@@ -19,25 +19,6 @@
 
 #include <rmn/test_helpers.h>
 
-void end_of_test(void){
-  fprintf(stderr,"==================== end of test  ==================== ") ;
-  system("date '+%Y-%m-%d_%H.%M.%S' 1>&2") ;
-}
-
-void end_of_test_notime(void){
-  fprintf(stderr,"==================== end of test  ==================== \n") ;
-}
-
-void start_of_test(char *name){
-  fprintf(stderr,"==================== test : %s ==================== ", name) ;
-  system("date '+%Y-%m-%d_%H.%M.%S' 1>&2") ;
-  atexit(end_of_test) ;
-}
-
-void start_of_test_notime(char *name){
-  fprintf(stderr,"==================== test : %s ==================== \n", name) ;
-  atexit(end_of_test_notime) ;
-}
 // common Fortran manglings
 void start_of_test_(char *name){ start_of_test(name) ; }
 void start_of_test__(char *name){ start_of_test(name) ; }
