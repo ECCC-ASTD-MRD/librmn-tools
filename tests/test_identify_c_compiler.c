0735cd51496a688affc339c6efb0bab629d36bb8
M. Valin <mfvalin@gmail.com>
2025-09-01 11:13:26 -0400

    adapted to identify_fc_compiler.h

--- tests/test_identify_c_compiler.c
+++ tests/test_identify_c_compiler.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <rmn/identify_c_compiler.h>
+#include <rmn/identify_fc_compiler.h>
 #include <rmn/test_helpers.h>
-
-char *identify_compiler() ;
-int identify_address_mode() ;
 
 // int main(int argc, char **argv){
 int main(){
   start_of_test("identify_c_compiler");
-  fprintf(stderr, "compiler = '%s', address mode = %d bits\n", C_COMPILER_NAME, ADDRESS_MODE) ;
-  fprintf(stderr, "identify_c_compiler() = '%s'\n", identify_c_compiler()) ;
-  fprintf(stderr, "identify_address_mode() = %d\n", identify_address_mode()) ;
+#if defined(C_COMPILER_ID)
+  fprintf(stderr, "compiler = '%s', address mode = %d bits\n", C_COMPILER_ID, ADDRESS_SIZE) ;
+#else
+  fprintf(stderr, "ERROR: C_COMPILER_ID is not defined\n") ;
+  exit(1) ;
+#endif
   return 0 ;
 }
--- tests/test_identify_fortran_compiler.F90
+++ tests/test_identify_fortran_compiler.F90
@@ -2,16 +2,21 @@
 
 program identify_compiler
   implicit none
-#include <rmn/identify_fortran_compiler.hf>
+#include <rmn/identify_fc_compiler.h>
 
   call start_of_test("is fortran compiler"//achar(0))
 #if defined(IN_FORTRAN_CODE)
   print *, "IN_FORTRAN_CODE is defined"
 #else
-  print *, "IN_FORTRAN_CODE is NOT defined"
+  print *, "ERROR : IN_FORTRAN_CODE is NOT defined"
+  call abort   ! IN_FORTRAN_CODE is NOT defined
 #endif
 
-  print *, "string = '"//fortran_compiler_name//"'"
-  print *, "macro  = '"//FORTRAN_COMPILER_NAME//"'"
-  print *, "address size =", ADDRESS_SIZE
+#if ! defined(FORTRAN_COMPILER_ID)
+  print *, 'ERROR : FORTRAN_COMPILER_ID is NOT defined'
+  call abort   ! FORTRAN_COMPILER_ID is NOT defined
+#else
+  print '(A,I3,A)', ' Fortran address size =', ADDRESS_SIZE,' bits'
+  print '(A)', ' Fortran compiler is '//FORTRAN_COMPILER_ID
+#endif
 end
