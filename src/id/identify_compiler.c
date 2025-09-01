caf5ba10c4597bd984d6c09c15dbbaea508aba8d
M. Valin <mfvalin@gmail.com>
2025-09-01 11:14:53 -0400

    adapted to identify_fc_compiler.h

--- src/id/identify_compiler.c
+++ src/id/identify_compiler.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <rmn/identify_c_compiler.h>
+#include <rmn/identify_fc_compiler.h>
 
 char *identify_c_compiler(){
-//  printf("compiler = '%s', address mode = %d bits\n", C_COMPILER, ADDRESS_MODE) ;
-  return C_COMPILER_NAME ;
+//  printf("compiler = '%s', address mode = %d bits\n", C_COMPILER_ID, ADDRESS_SIZE) ;
+  return C_COMPILER_ID ;
 }
 int identify_address_mode(){
-  return ADDRESS_MODE ;
+  return ADDRESS_SIZE ;
 }
