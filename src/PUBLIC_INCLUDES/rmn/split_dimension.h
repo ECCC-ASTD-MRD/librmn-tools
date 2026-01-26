92aaafa6fc3977d29736f33646c70c697f03e492
M. Valin <mfvalin@gmail.com>
2026-01-26 12:29:42 -0500

    fixing/testing corner cases

--- src/PUBLIC_INCLUDES/rmn/split_dimension.h
+++ src/PUBLIC_INCLUDES/rmn/split_dimension.h
@@ -14,14 +14,7 @@
 // Author:
 //     M. Valin,   Recherche en Prevision Numerique, 2025
 //
-#if ! defined(SPLIT_DIMENSION_H)
-#define SPLIT_DIMENSION_H
-
-// generic signed integer pair
-// typedef struct{
-//   int32_t i1 ;
-//   int32_t i2 ;
-// }int_pair ;              // pair of signed integers
+#if ! defined(AXIS_NULL)
 
 // index pair for 2D array
 typedef struct{
@@ -36,12 +29,13 @@ typedef struct{
   int32_t k  ;
 }index_trio ;            // 3D coordinate trio
 
-// description of a split array dimension (axis)
+// array split descriptor along a dimension (axis)
 typedef struct{
   int32_t nbk ;   // number of blocks along a dimension
   int16_t ln0 ;   // size of first block along a dimension
-  int16_t ln1 ;   // size of next blocks along a dimension
+  int16_t ln1 ;   // size of all the following blocks along a dimension
 } array_axis ;
+#define AXIS_NULL (array_axis){ .nbk=0, .ln0=0, .ln1=0 }
 
 // elements of an array block along a dimension
 typedef struct{
@@ -58,21 +52,28 @@ typedef struct{
 
 // block ordinal from index and sizes (along one dimension) (unsafe)
 // used by block_ordinal
-// l   [IN] : index along an array dimension
-// ln  [IN] : size of all but first block along a dimension
+// l   [IN] : index along an array dimension (origin 0)
+// ln1 [IN] : size of all blocks but first one along a dimension
 // ln0 [IN] : size of first block along a dimension
 // return block ordinal along that dimension
 static inline int32_t b_index(int32_t l, int32_t ln1, int32_t ln0){
-  return (l + ln1 - ln0)/ln1 ;
+  return (l < ln0) ? 0 : ((l + ln1 - ln0)/ln1) ;
+}
+// block ordinal from index and axis descriptor (uses b_index)
+// l    [IN] : index along an array dimension (origin 0)
+// axis [IN] : axis descriptor
+// return block ordinal along that dimension
+static inline int32_t axis_b_index(int32_t l, array_axis axis){
+  return b_index(l, axis.ln1, axis.ln0) ;
 }
 
-// get block ordinal that contains element number index along a dimension
-// axis  [IN] : axis description
-// index [IN] : index of array element along a dimension
+// get ordinal of block that contains element number index along a dimension
+// axis  [IN] : axis descriptor
+// index [IN] : position of array element along a dimension (origin 0)
 // return block ordinal containing requested element (-1 if error)
 static inline int32_t block_ordinal(array_axis axis,int32_t index){
   if(index < 0) return -1 ;                           // invalid index
-  int ordinal = b_index(index, axis.ln1, axis.ln0) ;
+  int ordinal = axis_b_index(index, axis) ;
   return (ordinal >= axis.nbk) ? -1 : ordinal ;       // chek for ordinal out of range (beyond last block)
 }
 
@@ -82,7 +83,16 @@ static inline int32_t block_ordinal(array_axis axis,int32_t index){
 // ln0 [IN] : size of first block along a dimension (ln/2 <= ln0 < 2*ln)
 // return index limits along a dimension for this block
 static inline index_range r_limits(int32_t bl, int32_t ln1, int32_t ln0){
+  if(bl < 0) return (index_range){.ix0 = 0 , .ixn = -1} ;  // return invalid range
   return (bl == 0) ? (index_range){.ix0 = 0 , .ixn = ln0-1} : (index_range){.ix0 = (bl-1)*ln1 + ln0, .ixn = bl*ln1 + ln0 -1 } ;
+}
+// index range from block index and axis descriptor (along one dimension) (unsafe)
+// bl  [IN] : block index along a dimension
+// axis [IN] : axis descriptor
+// return index limits along a dimension for this block
+static inline index_range axis_r_limits(int32_t bl, array_axis axis){
+  if(bl < 0 || bl > (axis.nbk-1)) return (index_range){.ix0 = 0 , .ixn = -1} ;  // return invalid range
+  return r_limits(bl, axis.ln1, axis.ln0) ;
 }
 // static inline index_pair b_limits(int32_t bl, int32_t ln1, int32_t ln0){
 //   return (bl == 0) ? (index_pair){.i   = 0 , .j   = ln0-1} :  (index_pair){.i    = (bl-1)*ln1 + ln0, .j   = bl*ln1 + ln0 -1 } ;
@@ -95,7 +105,8 @@ static inline index_range r_limits(int32_t bl, int32_t ln1, int32_t ln0){
 // { 0, -1 } is returned in case of errror
 static inline index_range block_limits(array_axis axis,int32_t ordinal){
   if((ordinal >= axis.nbk) || (ordinal < 0)) return (index_range) { .ix0 = 0, .ixn = -1 } ;
-  return r_limits(ordinal, axis.ln1 , axis.ln0) ;
+  return axis_r_limits(ordinal, axis) ;
+//   return r_limits(ordinal, axis.ln1 , axis.ln0) ;
 }
 
 // split n into pieces preferably of size bsize
@@ -106,37 +117,45 @@ static inline index_range block_limits(array_axis axis,int32_t ordinal){
 // if size is odd,  pieces will be >  bsize/2 or <= bsize + bsize/2
 // pieces will be smaller than the minimum only if n is also smaller
 static inline array_axis split_axis(int n, int bsize){
-  array_axis r = (array_axis){ .nbk=0, .ln0=-1, .ln1=-1 } ;
-  if(n <= 0 || bsize <= 0) goto end ;
-  r.nbk = (n + bsize/2) / bsize ;      // number of pieces
-  r.nbk = (r.nbk == 0) ? 1 : r.nbk ;   // cannot be less than 1
-  r.ln0 = n - (r.nbk - 1) * bsize ;    // size of first piece
-  r.ln1 = bsize ;
-end:
+  array_axis r ;
+  if(n > 0 && bsize > 0){
+    r.nbk = (n + bsize/2) / bsize ;      // number of pieces
+    r.nbk = (r.nbk == 0) ? 1 : r.nbk ;   // cannot be less than 1
+    r.ln0 = n - (r.nbk - 1) * bsize ;    // size of first piece
+    r.ln1 = (r.nbk == 1) ? 0 : bsize ;   // size of all subsequent pieces (0 if r.nbk == 1)
+  }else{
+    r = AXIS_NULL ;
+  }
   return r ;
 }
 
-// split n into pieces preferably of size bsize
-// n     [IN] : total number of items
-// bsize [IN] : requested size of pieces
+// split n into pieces preferably of size bsize (and a minimum size)
+// n       [IN] : total number of items
+// bsize   [IN] : requested size of pieces
+// minsize [IN] : minimum size of pieces
 // the first piece may be smaller or larger than the requested size
-// if size is even, pieces will be >= bsize/2 or <  bsize + bsize/2
-// if size is odd,  pieces will be >  bsize/2 or <= bsize + bsize/2
-// pieces will be smaller than the minimum only if n is also smaller
-// residual processing is performed as if bsize was 32
-static inline array_axis split_axis_32(int n, int bsize){
-  array_axis r = (array_axis){ .nbk=0, .ln0=-1, .ln1=-1 } ;
-  if(n <= 0 || bsize <= 0) goto end ;
-  r.nbk = n / bsize ;                  // number of pieces
-  r.ln0 = n - (bsize * r.nbk) ;        // residual
-  if(r.ln0 > 31) {                     // add a piece of size >= 32
-    r.nbk++ ;
+// pieces will be >= minsize or <  bsize + minsize -1
+// pieces will be smaller than minsize only if n is also smaller
+static inline array_axis split_axis_min(int n, int bsize, int minsize){
+  array_axis r ;
+  if(n > 0 && bsize > 0){
+    r.nbk = n / bsize ;                  // number of pieces
+    r.ln0 = n - (bsize * r.nbk) ;        // residual
+
+    if(r.ln0 >= minsize) {               // residual >= minsize
+      r.nbk++ ;                          // bump number of pieces
+
+    }else{                               // residual < minsize
+      if(r.nbk == 0){
+        r.nbk = 1 ;                      // number of pieces must be at least 1
+      }else{
+        r.ln0 += bsize;                  // size of first piece = residual + bsize
+      }
+    }
+    r.ln1 = (r.nbk == 1) ? 0 : bsize ;   // size of all subsequent pieces (0 if r.nbk == 1)
   }else{
-    r.nbk = (r.nbk == 0) ? 1 : r.nbk ; // cannot be less than 1
-    r.ln0 += bsize;                    // increment size of first piece by bsize
+    r = AXIS_NULL ;
   }
-  r.ln1 = bsize ;
-end:
   return r ;
 }
 
--- tests/test_split_dimension.c
+++ tests/test_split_dimension.c
@@ -24,21 +24,76 @@
 int main(int argc, char **argv){
   (void)(argc) ;
   (void)(argv) ;
-  int npts, ix, bsize, i, i0, in, ncheck ;
+  int npts, ix, bsize, i, i0, in, ncheck, minsize, j ;
   array_axis axis ;
   index_range range ;
+  int ax[] = { 7 , 9, 32, 39, 40, 41, 63, 64, 65, 72, 97 } ;
+  int bx[] = { 15, 32, 64, 95, 96, 127, 128, 223, 224 } ;
 
   start_of_test("dimension split test") ;
+
+  bsize = 32 ;
+  minsize = 8 ;
+  fprintf(stderr, "=============== blocks of size %d, minsize %d check ===============\n", bsize, minsize) ;
+  ncheck = sizeof(ax)/sizeof(int) ;
+  for(j = 0 ; j < ncheck ; j++){
+    axis = split_axis_min(ax[j], bsize, minsize) ;
+    fprintf(stderr, "n = %3d, nblks = %3d, l0 = %3d, ln = %3d", ax[j], axis.nbk, axis.ln0, axis.ln1) ;
+    for(i=-2 ; i<(axis.nbk+2) ; i++){    // i<0 and i>=axis.nbk are expected to return invalid block limits
+      range = block_limits(axis, i) ;
+      if(range.ix0 > range.ixn){
+        if(i < 0 || i >= axis.nbk) continue ;
+        goto fail ;
+      }
+      fprintf(stderr, " [%3d:%3d]", range.ix0, range.ixn) ;
+      for(ix=range.ix0 ; ix<=range.ixn ; ix++){
+        if(i != block_ordinal(axis, ix)){
+          fprintf(stderr, "\n ERROR: index = %d, expecting block ordinal %d, got %d", ix, i, block_ordinal(axis, ix));
+          goto fail ;
+        }
+      }
+    }
+    fprintf(stderr, "\n") ;
+  }
+
+  bsize = 64 ;
+  minsize = 32 ;
+  fprintf(stderr, "=============== blocks of size %d, minsize %d check ===============\n", bsize, minsize) ;
+  ncheck = sizeof(bx)/sizeof(int) ;
+  for(j = 0 ; j < ncheck ; j++){
+    axis = split_axis_min(bx[j], bsize, minsize) ;
+    fprintf(stderr, "n = %3d, nblks = %3d, l0 = %3d, ln = %3d", bx[j], axis.nbk, axis.ln0, axis.ln1) ;
+    for(i=-1 ; i<axis.nbk+1 ; i++){
+      range = block_limits(axis, i) ;
+      if(range.ix0 > range.ixn){
+        if(i < 0 || i >= axis.nbk) continue ;
+        goto fail ;
+      }
+      fprintf(stderr, " [%3d:%3d]", range.ix0, range.ixn) ;
+      for(ix=range.ix0 ; ix<=range.ixn ; ix++){
+        if(i != block_ordinal(axis, ix)){
+          fprintf(stderr, "\n ERROR: index = %d, expecting block ordinal %d, got %d", ix, i, block_ordinal(axis, ix));
+          goto fail ;
+        }
+      }
+    }
+    fprintf(stderr, "\n") ;
+  }
 
   bsize = 5 ;
   fprintf(stderr, "=============== 1..3 blocks of size 5 limits <--> ordinal check ===============\n") ;
 // block size 5, 1 to 3*bsize elements, verbose test of consistency of block_limits with block_ordinal
   for(npts=1 ; npts <= 3*bsize ; npts+=2){
-    axis = split_axis(npts, bsize) ;
-    fprintf(stderr, "n = %2d, nblks = %2d, l0 = %2d, ln = %2d", npts, axis.nbk, axis.ln0, axis.ln1) ;
-    for(i=0 ; i<axis.nbk ; i++){
+    axis = split_axis_min(npts, bsize, (bsize+1)/2) ;
+//     axis = split_axis(npts, bsize) ;
+    fprintf(stderr, "n = %3d, nblks = %3d, l0 = %3d, ln = %3d", npts, axis.nbk, axis.ln0, axis.ln1) ;
+    for(i=-1 ; i<axis.nbk+1 ; i++){
       range = block_limits(axis, i) ;
-      fprintf(stderr, " [%2d:%2d]", range.ix0, range.ixn) ;
+      if(range.ix0 > range.ixn){
+        if(i < 0 || i >= axis.nbk) continue ;
+        goto fail ;
+      }
+      fprintf(stderr, " [%3d:%3d]", range.ix0, range.ixn) ;
       for(ix=range.ix0 ; ix<=range.ixn ; ix++){
         if(i != block_ordinal(axis, ix)){
           fprintf(stderr, "\n ERROR: index = %d, expecting block ordinal %d, got %d", ix, i, block_ordinal(axis, ix));
@@ -50,14 +105,22 @@ int main(int argc, char **argv){
   }
   fprintf(stderr, "SUCCESS\n") ;
   fprintf(stderr, "=============== 1..5 blocks of size 1..16 limits <--> ordinal check ===============\n") ;
-// block size 4 to 16, 1 to 5*bsize elements, test consistency of block_limits with block_ordinal
-  for(bsize=1 ; bsize<17 ; bsize++){
+// block size 2 to 16, 1 to 5*bsize elements, test consistency of block_limits with block_ordinal
+  for(bsize=2 ; bsize<17 ; bsize++){
     for(npts=1 ; npts <= 5*bsize ; npts++){
-      axis = split_axis(npts, bsize) ;
+      axis = split_axis_min(npts, bsize, bsize/2) ;
+//       axis = split_axis(npts, bsize) ;
+//       fprintf(stderr, "n = %3d, nblks = %3d, l0 = %3d, ln = %3d", npts, axis.nbk, axis.ln0, axis.ln1) ;
       in = -1 ;
       ncheck = 0 ;
-      for(i=0 ; i<axis.nbk ; i++){
+      for(i=-1 ; i<axis.nbk+1 ; i++){
         range = block_limits(axis, i) ;
+        if(range.ix0 > range.ixn){
+          if(i < 0 || i >= axis.nbk) continue ;
+          fprintf(stderr, "ERROR : i = %d, nbk = %d, range - %3d:%3d\n", i, axis.nbk, range.ix0, range.ixn) ;
+          goto fail ;
+        }
+//         fprintf(stderr, " [%3d:%3d]", range.ix0, range.ixn) ;
         i0 = range.ix0 ;
         if(i0 != in+1){      // hole/overlap in range
           fprintf(stderr, "ERROR (n = %d, bsize %d) : expecting start of block %d at %d, got %d\n", npts, bsize, i, in+1, i0) ;
@@ -72,6 +135,7 @@ int main(int argc, char **argv){
           }
         }
       }
+//       fprintf(stderr, "\n") ;
       if(npts != ncheck){
         fprintf(stderr, "ERROR: n = %d, bsize %d, checked %d\n", npts, bsize, ncheck) ;
         goto fail ;
