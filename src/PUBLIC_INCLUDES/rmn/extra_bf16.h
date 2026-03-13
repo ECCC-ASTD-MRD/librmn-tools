2687087faa8b3c8207cf4dfcf225f11ebadf9068
M. Valin <mfvalin@gmail.com>
2026-03-13 10:32:05 -0400

    compensate for missing __bf16 type in some compilers

--- src/PUBLIC_INCLUDES/rmn/extra_bf16.h
+++ src/PUBLIC_INCLUDES/rmn/extra_bf16.h
@@ -1,6 +1,6 @@
 // temporarily compensate for lack of __bf16 definition in some compilers
-#if defined(__PGI) && ! defined(__PGI__BF16__)
-#define __PGI__BF16__
+#if defined(__PGI) && ! defined(__EXTRA__BF16__)
+#define __EXTRA__BF16__
   typedef struct{
     uint16_t v ;
   } __bf16 ;
