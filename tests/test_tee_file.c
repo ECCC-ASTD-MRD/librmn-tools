3bfdc1c2cbb8fad2977a0697cf2defca0abd89d2
M. Valin <mfvalin@gmail.com>
2026-03-17 13:52:10 -0400

    getting App as an option

--- tests/test_tee_file.c
+++ tests/test_tee_file.c
@@ -16,21 +16,35 @@ void print_diag(FILE *f, char *what, int level){
 }
 #endif
 
+void dummy(char *msg){
+  TEE_FPRINTF(stderr, TEE_DEBUG, "in dummy");
+  Lib_Log(APP_LIBRMN, TEE_INFO, "%s\n", msg);
+}
+
 int main(int argc, char **argv){
-  (void)(argc) ;
+//   (void)(argc) ;
   start_of_test(argv[0]);
-  TEE_FPRINTF(stderr, -1, "MUST NOT SEE : %d %d %d\n", 10, 20, 30) ;
-  TEE_FPRINTF(stderr, 1, "MUST SEE : %d %d %d\n", 10, 20, 30) ;
+  set_msg_level(TEE_EXTRA) ;
+  set_tee_msg_level(TEE_EXTRA) ;
+  if(argc > 1){
+    set_use_app(1) ;
+    App_LogLevelNo(APP_DEBUG) ;
+  }
+  dummy("dummy's message") ;
+
+  tee_file = get_tee_file() ;
+  TEE_FPRINTF(stderr, TEE_EXTRA, "MUST NOT SEE : %d %d %d\n", 10, 20, 30) ;
+  TEE_FPRINTF(stderr, TEE_INFO, "MUST SEE : %d %d %d\n", 10, 20, 30) ;
   FILE *f1 = open_tee_file("test_tee_file.log") ;
   if(f1 == NULL) exit(1) ;
   tee_file = get_tee_file() ;
   if(tee_file == stdout) fprintf(stderr,"tee file is stdout\n") ;
-  TEE_FPRINTF(stderr, -1, "MUST NOT SEE : %d %d %d\n", 1, 2, 3) ;
-  TEE_FPRINTF(stderr, 1, "MUST SEE : %d %d %d\n", 1, 2, 3) ;
+  TEE_FPRINTF(stderr, TEE_EXTRA, "MUST NOT SEE : %d %d %d\n", 1, 2, 3) ;
+  TEE_FPRINTF(stderr, TEE_WARNING, "MUST SEE : %d %d %d\n", 1, 2, 3) ;
   FILE *f2 = open_tee_file(NULL) ;
   if(f2 == NULL) exit(1) ;
   tee_file = get_tee_file() ;
   if(tee_file == stdout) fprintf(stderr,"tee file is stdout\n") ;
-  TEE_FPRINTF(stderr, -1, "MUST NOT SEE : %d %d %d\n", 4, 5, 6) ;
-  TEE_FPRINTF(stderr, 1, "MUST SEE : %d %d %d\n", 4, 5, 6) ;
+  TEE_FPRINTF(stderr, TEE_EXTRA, "MUST NOT SEE : %d %d %d\n", 4, 5, 6) ;
+  TEE_FPRINTF(stderr, TEE_ERROR, "MUST SEE : %d %d %d\n", 4, 5, 6) ;
 }
