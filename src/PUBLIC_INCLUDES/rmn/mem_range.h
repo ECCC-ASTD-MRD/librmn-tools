43f9f707e9af943ad417967558972574abce58c3
M. Valin <mfvalin@gmail.com>
2026-08-01 19:20:22 -0400

    saving work, name change

--- src/PUBLIC_INCLUDES/rmn/mem_range.h
+++ src/PUBLIC_INCLUDES/rmn/mem_range.h
@@ -47,8 +47,8 @@
 #define RANGE(KIND) KIND##_range
 #define NO_RANGE {NULL, NULL}
 #define RANGE_NULL(KIND) ((RANGE(KIND))NO_RANGE)
-#define RANGE_NULL_VAR(KIND) KIND##_range_null
-#define RANGE_NULL_CONST(KIND) static const RANGE(KIND) RANGE_NULL_VAR(KIND) = RANGE_NULL(KIND) ;
+#define RANGE_NULL_NAME(KIND) KIND##_range_null
+#define RANGE_NULL_CONST(KIND) static const RANGE(KIND) RANGE_NULL_NAME(KIND) = RANGE_NULL(KIND) ;
 // #define RANGE_TYPEDEF(KIND) typedef struct{ KIND *bot, *top ; } RANGE(KIND) ; static const RANGE(KIND) KIND##_range_null = RANGE_NULL(KIND) ;
 #define RANGE_TYPEDEF(KIND) typedef struct{ KIND *bot, *top ; } RANGE(KIND) ;
 #define RANGE_KIND(KIND,BOT,TOP) ( (RANGE(KIND)) { (void *)(BOT) , (void *)(TOP) } )
